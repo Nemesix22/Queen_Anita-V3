ca8bbc256913d3c82c5fb97459b3ef16a0fe8a9e
Nemesix22 <nemesisuzumaki456@gmail.com>
2025-04-21 10:06:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,25 +9,25 @@ global.location = "Nigeria, Lagos" //ur location
 
 //new
 global.botname = 'QUEEN_ANITA-V3' //ur bot name
-global.ownernumber = ' ' //ur owner number
-global.ownername = "DAVID CYRIL"  //ur owner name
+global.ownernumber = '2348077825595' //ur owner number
+global.ownername = "死_ＮＥＭＥＳ１Ｘ♠️"  //ur owner name
 global.websitex = "http://www.youtube.com/@DavidCyril_TECH"
 global.wagc = "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L"
 global.themeemoji = '👸'
 global.wm = "DAVID CYRIL TECH"
 global.botscript = 'https://github.com/DeeCeeXxx/Queen_Anita-V3' //script link
 global.packname = "STICKER BY"
-global.author = "QUEEN_ANITA-V3"
-global.creator = "2347043759577@s.whatsapp.net"
-global.xprefix = "."
-global.premium = ["2347043759577"] // Premium User
+global.author = "死_ＮＥＭＥＳ１Ｘ♠️"
+global.creator = "2348077825585@s.whatsapp.net"
+global.xprefix = "X"
+global.premium = ["2348077825595"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
 global.typereply = 'v3' // reply type 'v1' => 'v4'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.autoblocknumber = '' //set autoblock country code
+global.antiforeignnumber = '' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = true //auto status/story view
